5b048774ccf2e2690532498dffdc39c2ab653b71
portakalkoku <portakalkoku@gmail.com>
2022-04-24 02:36:29 +0300

    define public request method for API helper

--- betting/Service/API.swift
+++ betting/Service/API.swift
@@ -11,6 +11,30 @@ public protocol TargetType {
     var parameters: [String: String] { get }
 }
 
+extension API {
+    class func request(type: TargetType, completion: @escaping (Result<Data, APIError>) -> ()) {
+        let urlEncoder = URLEncodedFormParameterEncoder(encoder: URLEncodedFormEncoder(), destination: .queryString)
+        
+        var parameters = type.parameters
+        parameters["apiKey"] = sharedInstance.apiKey
+        
+        let url = sharedInstance.baseUrl.appendingPathComponent(type.url)
+        
+        AF.request(url,
+                   method: .get,
+                   parameters: parameters,
+                   encoder: urlEncoder).validate().responseData { response in
+            switch response.result {
+            case .success(let data):
+                let dataStr = String(decoding: data, as: UTF8.self)
+                print("⏩\(dataStr)")
+                completion(.success(data))
+            case .failure(_):
+                completion(.failure(.serviceError))
+            }
+        }
+    }
+}
 enum APIError: Error {
     case wrongMapping
     case serviceError
