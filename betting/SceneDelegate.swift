833a50bc25bf7341e816015db3dea34d4cb4dc95
portakalkoku <portakalkoku@gmail.com>
2022-04-25 00:33:03 +0300

    style navigation bar

--- betting/SceneDelegate.swift
+++ betting/SceneDelegate.swift
@@ -14,6 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let viewController = BulletinViewController.build(api: API())
         let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.topItem?.title = "Best Bet"
+        navigationController.navigationBar.backgroundColor = .white
+        navigationController.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.black]
         window?.rootViewController = navigationController
     }
 }
