b12726bfc2b8e05bd3e3218756900194484f53d2
portakalkoku <portakalkoku@gmail.com>
2022-04-25 01:56:34 +0300

    add collectionview for odds of event

--- betting/Screens/Bulletin/Views/EventCell.swift
+++ betting/Screens/Bulletin/Views/EventCell.swift
@@ -9,15 +9,28 @@ import UIKit
 
 class EventCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.register(.init(nibName: "OddCell", bundle: nil), forCellWithReuseIdentifier: "OddCell")
+            collectionView.delegate = self
+            collectionView.dataSource = self
+        }
     }
     
+    func reloadWith(data: String) {
+        eventNameLabel.text = data
+        collectionView.reloadData()
+    }
+}
+
+extension EventCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OddCell", for: indexPath)
+        return cell
+    }
 }
