2191a0e5e6132c48216fb3b08530d2444ca0fe07
portakalkoku <portakalkoku@gmail.com>
2022-04-24 13:25:40 +0300

    define builder for bulletinviewcontroller

---
+++ betting/Screens/Bulletin/ViewController/BulletinViewController+Build.swift
@@ -0,0 +1,18 @@
+//
+//  BulletinViewController+Build.swift
+//  betting
+//
+//  Created by Çağrı Portakalkökü on 24.04.2022.
+//
+
+import Foundation
+import UIKit
+
+extension BulletinViewController {
+    class func build(api: API) -> UIViewController {
+        let viewModel = BulletinViewModel(api: api)
+        let viewController = BulletinViewController(viewModel: viewModel)
+        viewModel.delegate = viewController
+        return viewController
+    }
+}
