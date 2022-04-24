f041fc0816942b792e3bf5aa0328e94bb83fe5ad
portakalkoku <portakalkoku@gmail.com>
2022-04-25 00:33:31 +0300

    define event cell

---
+++ betting/Screens/Bulletin/Views/EventCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventCell.swift
+//  betting
+//
+//  Created by Çağrı Portakalkökü on 24.04.2022.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
