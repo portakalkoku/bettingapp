9477926ee2ef81bc6a94d4719ad1410f9322c664
portakalkoku <portakalkoku@gmail.com>
2022-04-25 00:31:40 +0300

    define odds models

--- betting/Screens/Bulletin/Model/BulletinModels.swift
+++ betting/Screens/Bulletin/Model/BulletinModels.swift
@@ -6,20 +6,58 @@
 //
 
 import Foundation
+import UIKit
 
 enum BulletinModels {
-    struct Sport: Codable{
+    struct Sport: Codable {
         let key: String
         let group: String
         let title: String
         let description: String
         let active: Bool
     }
+    
+    struct GroupCellModel {
+        let image: String
+        let title: String
+        let selected: Bool
+    }
+    
+    struct Odds: Codable {
+        let sport_key: String
+        let commence_time: String
+        let home_team: String
+        let away_team: String
+        let bookmakers: [Bookmaker]
+     
+        
+        struct Bookmaker: Codable {
+            let key: String
+            let title: String
+            let markets: [Market]
+        }
+        
+        struct Market: Codable {
+            let key: String
+            let outcomes: [Outcome]
+        }
+        
+        struct Outcome: Codable {
+            let name: String
+            let price: Double
+        }
+    }
 }
 
-enum PredefinedSports: String, Codable {
+enum SportType: String, Codable {
     case soccer = "Soccer"
     case basketball = "Basketball"
     case baseball = "Baseball"
     case americanFootball = "American Football"
+    case rugby = "Rugby League"
+    case golf = "Golf"
+    case martialArts = "Mixed Martial Arts"
+    case aussieFootball = "Aussie Rules"
+    case iceHockey = "Ice Hockey"
+    case cricket = "Cricket"
 }
