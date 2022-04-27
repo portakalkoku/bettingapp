bf0e657c65e3da9e6dfda7904374a3b691969cf2
portakalkoku <portakalkoku@gmail.com>
2022-04-28 01:29:55 +0300

    implement tableview delegates

--- betting/Screens/Checkout/ViewController/CheckoutViewController.swift
+++ betting/Screens/Checkout/ViewController/CheckoutViewController.swift
@@ -1,6 +1,19 @@
     override func viewDidLoad() {
         hideKeyboardWhenTappedAround()
     }
+extension CheckoutViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.getCartEvents().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventTableViewCell") as! EventTableViewCell
+        cell.reloadWith(event: viewModel.getCartEvents()[indexPath.row])
+        cell.delegate = self
+        return cell
+        
+    }
+}
 extension CheckoutViewController: CheckoutViewModelDelegate {
     func reloadTableView() {
         emptyView.isHidden = true
