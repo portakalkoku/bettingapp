3e037144219855dc9a188fe216a6228f87cfd0e7
portakalkoku <portakalkoku@gmail.com>
2022-04-28 01:31:13 +0300

    style event table view cell

---
+++ betting/Screens/Checkout/Views/EventTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  EventTableViewCell.swift
+//  betting
+//
+//  Created by Çağrı Portakalkökü on 27.04.2022.
+//
+
+import UIKit
+protocol EventTableViewCellDelegate: AnyObject {
+    func didTapRemoveEventFromCart(event: CartModels.Event)
+}
+
+class EventTableViewCell: UITableViewCell {
+    @IBOutlet weak var matchNameLabel: UILabel!
+    @IBOutlet weak var oddLabel: UILabel!
+    @IBOutlet weak var ratioLabel: UILabel!
+    @IBOutlet weak var removeFromCart: UIButton!
+    
+    private var event: CartModels.Event?
+    weak var delegate: EventTableViewCellDelegate?
+    func reloadWith(event: CartModels.Event) {
+        self.event = event
+        matchNameLabel.text = event.name
+        switch event.side {
+        case .away:
+            oddLabel.text = "2"
+        case .draw:
+            oddLabel.text = "0"
+        case .home:
+            oddLabel.text = "1"
+        }
+        ratioLabel.text = "\(event.price)"
+    }
+    
+    @IBAction func removeEventFromCart(_ sender: Any) {
+        guard let event = event else {
+            return
+        }
+
+        delegate?.didTapRemoveEventFromCart(event: event)
+    }
+}
