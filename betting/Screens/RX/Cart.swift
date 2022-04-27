7c277638cb20b844833a76607f8f07b07c8c2e4e
portakalkoku <portakalkoku@gmail.com>
2022-04-27 04:21:29 +0300

    create cart rx

---
+++ betting/Screens/RX/Cart.swift
@@ -0,0 +1,34 @@
+//
+//  Cart.swift
+//  betting
+//
+//  Created by Çağrı Portakalkökü on 27.04.2022.
+//
+
+import RxCocoa
+
+class Cart {
+    var events: BehaviorRelay<[CartModels.Event]> = .init(value: [])
+    func addOrRemoveEvent(_ event: CartModels.Event) {
+        var events = self.events.value
+        if let index = events.firstIndex(where: {$0.id == event.id}) {
+            events.remove(at: index)
+        } else {
+            events.append(event)
+        }
+        self.events.accept(events)
+    }
+    
+    func checkIfOddExistInCart(_ id: String, type: BulletinModels.OddType) -> Bool {
+        let events = self.events.value
+        return events.firstIndex(where: {$0.id == id && $0.side == type }) != nil
+    }
+    
+    func getMultiplier() -> Double {
+        var multiplier = 1.0
+        events.value.forEach { event in
+            multiplier = multiplier * event.price
+        }
+        return multiplier
+    }
+}
