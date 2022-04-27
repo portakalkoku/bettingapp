74638ffe2598c02c5ce92ceb5d96f16f7af0e2bf
portakalkoku <portakalkoku@gmail.com>
2022-04-28 01:29:39 +0300

    implement viewmodeldelegate

--- betting/Screens/Checkout/ViewController/CheckoutViewController.swift
+++ betting/Screens/Checkout/ViewController/CheckoutViewController.swift
@@ -1,3 +1,21 @@
     override func viewDidLoad() {
         hideKeyboardWhenTappedAround()
     }
+extension CheckoutViewController: CheckoutViewModelDelegate {
+    func reloadTableView() {
+        emptyView.isHidden = true
+        stackView.isHidden = false
+        tableView.isHidden = false
+        tableView.reloadData()
+    }
+
+    func showEmptyScreen() {
+        emptyView.isHidden = false
+        stackView.isHidden = true
+        tableView.isHidden = true
+    }
+    
+    func reloadMultiplier(with value: Double) {
+        maxReturnLabel.text = value.convertToTwoDecimalString()
+    }
+}
