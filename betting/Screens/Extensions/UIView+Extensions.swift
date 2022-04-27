8f49f447f38fa0b57b48f7a9403c3cc7a488b32a
portakalkoku <portakalkoku@gmail.com>
2022-04-27 04:20:42 +0300

    define new extension to load nibs easily

---
+++ betting/Screens/Extensions/UIView+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+Extensions.swift
+//  betting
+//
+//  Created by Çağrı Portakalkökü on 27.04.2022.
+//
+
+import UIKit
+
+extension UIView {
+    func loadNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nibName = type(of: self).description().components(separatedBy: ".").last!
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as! UIView
+    }
+}
