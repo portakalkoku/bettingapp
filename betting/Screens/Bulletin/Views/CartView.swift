acc831fdc5dbd20cd45535372632c9c4c894f063
portakalkoku <portakalkoku@gmail.com>
2022-04-27 04:18:38 +0300

    init cartview

---
+++ betting/Screens/Bulletin/Views/CartView.swift
@@ -0,0 +1,30 @@
+//
+//  CartView.swift
+//  betting
+//
+//  Created by Çağrı Portakalkökü on 27.04.2022.
+//
+
+import UIKit
+
+protocol CartViewDelegate {
+}
+
+class CartView: UIView {
+    var cartContentView: CartContentView? = nil
+    override func awakeFromNib() {
+        isHidden = true
+        guard let cartContentView = CartContentView().loadNib() as? CartContentView else { return }
+        self.cartContentView = cartContentView
+        self.addSubview(cartContentView)
+    }
+    
+    func reloadViewWith(_ multiplier: Double) {
+        if multiplier <= 1.0 {
+            isHidden = true
+        } else {
+            isHidden = false
+        }
+        cartContentView?.reloadMultiplier(multiplier: multiplier)
+    }
+}
